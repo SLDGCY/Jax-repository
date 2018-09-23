8c8c7234fa149a3596dc583196eccae931ca7b2b
SLDGCY <1285311797@qq.com>
2018-09-23 16:55:33 +0800

    add REAADME

---
+++ 新建文件夹/05.py
@@ -0,0 +1,21 @@
+# result = (50 - 5*6) / 4
+# result = 2+2
+# result = 8 / 5
+# result = 17 / 3
+# result = 17 // 3
+# result = 17 % 3
+# result = 5 * 3 + 2
+# result = 5 ** 2
+# result = 2 ** 7
+# width = 20
+# height = 5 * 9
+# result = 4 * 3.75 - 1
+nihao = [1,2,4,5,3,]
+jiashu =[1,2,2,2,2,]
+# print(result)
+for a in nihao:
+    print('123:', a)
+for i in range(5):
+
+    # print('i=',i,' nihao[i]=',nihao[i])
+    print(nihao[i]+jiashu[i])
---
+++ 新建文件夹/README.md
@@ -0,0 +1,5 @@
+# pytgon课堂练习
+-----------
+#### 180923
+- if else github
+- python基础运用
